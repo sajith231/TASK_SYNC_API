c63d98cbe3634ada74da14daf8390b85c22f7761
sajith231 <sajiththomas231@gmail.com>
2026-01-20 14:07:22 +0530

    acc_sales_type

--- acc_sales_type/urls.py
+++ acc_sales_type/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/upload-acc-sales-types/", views.upload_acc_sales_types),
-    path("api/get-acc-sales-types/", views.get_acc_sales_types),
+    path("upload-acc-sales-types/", views.upload_acc_sales_types),
+    path("get-acc-sales-types/", views.get_acc_sales_types),
 ]
